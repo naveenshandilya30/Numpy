8556039531341e46b2885668b92b55bf9b9074e7
naveenshandilya <naveenshandilya30@gmail.com>
2018-07-06 00:20:43 +0530

    assignment 19

---
+++ Assignment_19.py
@@ -0,0 +1,41 @@
+#Question 1
+
+import numpy as np
+a=np.random.randint(1,10,10)
+print(a)
+print(np.mean(a))
+
+
+#Question 2
+
+import numpy as np
+
+a=np.random.randint(1,20,20)
+print(a)
+print(np.var(a))
+print(np.std(a))
+
+
+
+#Question 3
+
+import numpy as np
+a=np.random.random((10,20))
+b=np.random.random((20,25))
+x=np.dot(a,b)
+print(x)
+print(np.sum(x))
+
+
+
+#Question 4
+
+import numpy as np
+import math
+a=np.random.random((10,1))
+print(a)
+b=[]
+for i in range (0,10):
+    x=1/(1+math.exp(-(a[i,0])))
+    b.append(x)
+print(b)
